2bb5fea848f2f0bceadbb477fd02937c3ab78683
flakeparadigm <tyler@nienhouse.net>
2014-04-13 02:32:46 -0700

    Starting PathFinding for Agents > Added goHere class to agents. Does nothing yet > Created PathFinder class. Will be putting the pathfinding algorithm here. Probably going to use A* because it's efficient and documented.

--- Sciencers/src/model/Agent.java
+++ Sciencers/src/model/Agent.java
@@ -6,7 +6,7 @@ public class Agent {
 	
 	// agent id tracking
 	private static int currentId = 0;
-	public final int myID;
+	public final int MY_ID;
 	
 	// current status
 	private int hunger, fatigue;
@@ -15,7 +15,15 @@ public class Agent {
 	// player traits
 	private int intelligence, motivation, speed, strength;
 	
+	// constants
+	private final int AGENT_HEIGHT = 2;
+	
 	public Agent() {
-		myID = currentId++;
+		MY_ID = currentId++;
+	}
+	
+	public boolean goHere(Point destination) {
+		PathFinder thePath = new PathFinder(currentPosition, destination);
+		return false;
 	}
 }
---
+++ Sciencers/src/model/PathFinder.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.awt.Point;
+
+public class PathFinder {
+
+	public PathFinder(Point currentPosition, Point destination) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
