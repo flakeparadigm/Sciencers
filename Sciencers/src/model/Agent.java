46b27d140efb398e89fb5e33482c1729fc94e2a1
Flakeparadigm <tyler@nienhouse.net>
2014-04-15 20:47:25 -0700

    Agent update > basic tick update stuff > getting path with goHere method.

--- Sciencers/src/model/Agent.java
+++ Sciencers/src/model/Agent.java
@@ -11,27 +11,55 @@ public class Agent extends OurObservable {
 	private static int currentId = 0;
 	public final int MY_ID;
 	
-	// current status
-	private int hunger, fatigue;
+	// current status. Ints represent 100.0%
+	private int hunger = 1000, fatigue = 1000;
+	private boolean isWorking = false;
 	private Point currentPosition, targetPosition;
-	private Stack movements;
+	private Stack<Point> movements;
 	private Terrain terrain;
 	
 	// player traits
 	private int intelligence, motivation, speed, strength;
 	
+	// misc
+	private boolean evenTick = false;
+	
 	// Magic Numbers
 	private final int AGENT_HEIGHT = 2;
+	private final int MAX_WORKING_HUNGER = 250; // 25.0%
+	private final int MAX_WORKING_FATIGUE = 200; // 20.0%
 	
 	public Agent(Terrain terrain) {
 		MY_ID = currentId++;
 		this.terrain = terrain;
 	}
 	
+	public void update() {
+		// update stats
+		if(evenTick) {
+			if(isWorking) {
+				hunger -= 6;
+				fatigue -= 3;
+			} else {
+				hunger -= 1;
+				fatigue -= 1;
+			}
+		}
+		
+		if(!movements.isEmpty())
+			currentPosition = movements.pop();
+		
+		evenTick = !evenTick;
+	}
+	
 	// this will be what the agent uses to get the path to its next destination.
 	// should develop it out later to take in a building or something similar as an arg.
 	public boolean goHere(Point destination) {
 		PathFinder thePath = new PathFinder(currentPosition, destination, terrain, Tile.Sky);
-		return false;
+		movements = thePath.getPath();
+		if(movements.isEmpty()){
+			return false;
+		}
+		return true;
 	}
 }
