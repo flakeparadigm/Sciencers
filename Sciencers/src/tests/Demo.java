25c758d541d62a5b1f8f1066a463c8ac67f1613c
flakeparadigm <tyler@nienhouse.net>
2014-04-18 01:05:24 -0700

    Terrain Update Demo > A test of the world view's ability to update when the terrain is altered.

--- Sciencers/src/tests/Demo.java
+++ Sciencers/src/tests/Demo.java
@@ -1,26 +1,31 @@
 package tests;
 
+import java.awt.FlowLayout;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 import model.World;
+import view.Tile;
 import view.WorldView;
 
 public class Demo extends JFrame {
-	
+
 	// important things
 	private static World world;
 	static Toolkit tk = Toolkit.getDefaultToolkit();
-	
+
 	// Graphical elements
 	private static WorldView worldView;
 	private static Demo demoTools;
-	
+
 	// Test Buttons
 	private JButton testTerrainUpdateButton = new JButton("Test Terrain Update");
-	
+
 	// magic numbers
 	private final int X_SCREEN_SIZE = ((int) tk.getScreenSize().getWidth());
 	private final int Y_SCREEN_SIZE = ((int) tk.getScreenSize().getHeight());
@@ -30,7 +35,7 @@ public class Demo extends JFrame {
 	public static void main(String[] args) {
 		worldView = new WorldView();
 		worldView.setVisible(true);
-		
+
 		demoTools = new Demo();
 		demoTools.setVisible(true);
 	}
@@ -45,32 +50,59 @@ public class Demo extends JFrame {
 	private void setupProperties() {
 		worldView.setSize(X_SCREEN_SIZE - X_WINDOW_SIZE - 150, Y_WINDOW_SIZE);
 		worldView.setTitle("Sciencers Demo");
-		
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("Demo Controls");
 		setSize(X_WINDOW_SIZE, Y_WINDOW_SIZE);
-		setLayout(null);
+		setLayout(new FlowLayout());
 		setLocation(X_SCREEN_SIZE - (X_WINDOW_SIZE + 50), 50);
 	}
 
 	private void setupModel() {
 		world = worldView.getWorld();
-		//for example: add agent and farm
+		// for example: add agent and farm
 		world.addAgent(11);
 		world.addFarm(27, 16);
 	}
 
 	private void addComponents() {
-		
+		this.add(testTerrainUpdateButton);
 	}
-	
+
 	private void registerListeners() {
-		
+		testTerrainUpdateButton.addActionListener(new demoButtonListener());
 	}
-	
-	// PUT ALL TEST CASES BELOW
-	private void testTerrainUpdate() {
-		
+
+	// PUT ALL TEST CASES BELOW.
+	private class demoButtonListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent event) {
+			if (event.getSource() == testTerrainUpdateButton)
+				testTerrainUpdate();
+
+		}
+
+		private void testTerrainUpdate() {
+			JOptionPane.showMessageDialog(worldView, "This demo should replace 50 blocks along the top of the map with Stone (X)");
+			Runnable r = new Runnable() {
+				public void run() {
+					try {
+						for (int i = 0; i < 100; i += 2) {
+							world.getTerrain().updateTile(Tile.Stone, i, 0);
+							Thread.sleep(100);
+						}
+
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			};
+
+			new Thread(r).start();
+		}
+
 	}
 
 }
