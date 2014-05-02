19c4afe7ce43a6fccb11619022eb6a763d98d520
Flakeparadigm <tyler@nienhouse.net>
2014-05-01 17:13:11 -0700

    Serialization Entity, projectile, research

--- Sciencers/src/model/Entity.java
+++ Sciencers/src/model/Entity.java
@@ -2,8 +2,9 @@ package model;
 
 import java.awt.Dimension;
 import java.awt.geom.Point2D;
+import java.io.Serializable;
 
-public interface Entity {
+public interface Entity extends Serializable {
 	public void update();
 	public Point2D getPos();
 	public Dimension getSize();
