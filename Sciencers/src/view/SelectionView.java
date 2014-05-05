b64d59b0d53f7dd759553ab20eb20919d26b7d32
flakeparadigm <tyler@nienhouse.net>
2014-05-04 20:45:06 -0700

    SelectionView + Visual Feedback 1 There is now visual feedback for the SelectionView's rectangle selection. For point selection it highlights a single point for now, but we can add onto this later if we want to put little building previews in.

--- Sciencers/src/view/SelectionView.java
+++ Sciencers/src/view/SelectionView.java
@@ -2,105 +2,171 @@ package view;
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 public class SelectionView extends JPanel {
-	
+	private SelectionView myView;
 	private boolean waitingForRectangle = false;
 	private boolean waitingForPoint = false;
 	private Point firstPoint;
 	private Point secondPoint;
 	private Point currPoint;
-	
+
+	private Image selectionImg;
+
 	public SelectionView() {
-		SelectListener sl= new SelectListener();
+		myView = this;
+		try {
+			selectionImg = ImageIO.read(new File("imgs/Selection.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		SelectListener sl = new SelectListener();
 		this.addMouseListener(sl);
 		this.addMouseMotionListener(sl);
 	}
-	
+
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		Graphics2D g2 = (Graphics2D) g;
+
+		if (waitingForRectangle) {
+			if (firstPoint == null) {
+				g2.drawImage(selectionImg, currPoint.x * WorldView.TILE_SIZE,
+						currPoint.y * WorldView.TILE_SIZE, WorldView.TILE_SIZE,
+						WorldView.TILE_SIZE, null);
+			} else if(secondPoint == null) {
+				paintBox(g2, firstPoint, currPoint);
+			} else {
+				paintBox(g2,firstPoint,secondPoint);
+			}
+		} else if(waitingForPoint) {
+			
+		}
 	}
 	
+	private void paintBox(Graphics2D g2, Point startPoiont, Point endPoint) {
+		int currX = startPoiont.x;
+		int currY = startPoiont.y;
+		
+		 while(currX != (endPoint.x+1)) {
+			 while(currY != (endPoint.y+1)) {
+				 
+				// draw the image!
+				g2.drawImage(selectionImg, currX * WorldView.TILE_SIZE,
+						currY * WorldView.TILE_SIZE, WorldView.TILE_SIZE,
+						WorldView.TILE_SIZE, null);
+				
+				// increment the y point and do it again!
+				if(currY < (currPoint.y+1))
+					currY++;
+				else
+					currY--;
+				
+			}
+			
+			// reset currY
+			currY = startPoiont.y;
+			
+			// increment the x point and do it again!
+			if(currX < (endPoint.x+1))
+				currX++;
+			else
+				currX--;
+			
+		}
+	}
+
 	public Point getPoint() {
 		// reset point variables
 		firstPoint = null;
 		waitingForPoint = true;
-		
+
 		// wait for two points
-		while(waitingForPoint) {
-			if(firstPoint != null)
+		while (waitingForPoint) {
+			if (firstPoint != null)
 				waitingForPoint = false;
 		}
-		return new Point(firstPoint.x/WorldView.TILE_SIZE, firstPoint.y/WorldView.TILE_SIZE);
+		return firstPoint;
 	}
-	
+
 	public Rectangle getRectangle() {
-		
+
 		// reset point variables
 		firstPoint = null;
 		secondPoint = null;
 		waitingForRectangle = true;
-		
+
 		// wait for two points
-		while(waitingForRectangle) {
-			if(firstPoint != null && secondPoint != null)
+		while (waitingForRectangle) {
+			if (firstPoint != null && secondPoint != null)
 				waitingForRectangle = false;
 		}
-		
 
-		//set top left point and dimensions using two points
-		int x1 = firstPoint.x/WorldView.TILE_SIZE;
-		int x2 = secondPoint.x/WorldView.TILE_SIZE;
-		int y1 = firstPoint.y/WorldView.TILE_SIZE;
-		int y2 = secondPoint.y/WorldView.TILE_SIZE;
+		// set top left point and dimensions using two points
+		int x1 = firstPoint.x;
+		int x2 = secondPoint.x;
+		int y1 = firstPoint.y;
+		int y2 = secondPoint.y;
 
 		int x = Math.min(x1, x2);
 		int y = Math.max(y1, y2);
-		
+
 		int dx = Math.abs(x2 - x1);
 		int dy = Math.abs(y2 - y1);
-		
-		//make a rectangle using those
+
+		// make a rectangle using those
 		return new Rectangle(x, y, dx, dy);
 	}
 
 	public void update() {
 		repaint();
 	}
-	
-	private class SelectListener extends MouseAdapter implements MouseMotionListener {
+
+	private class SelectListener extends MouseAdapter implements
+			MouseMotionListener {
 
 		@Override
 		public void mouseClicked(MouseEvent e) {
-			System.out.println("Point clicked at pixel: " + e.getPoint().x + ", " + e.getPoint().y + 
-					" | Tile: " + e.getPoint().x/WorldView.TILE_SIZE + ", " + e.getPoint().y/WorldView.TILE_SIZE);
-			if(waitingForRectangle && firstPoint == null) {
-				firstPoint = e.getPoint();
-			} else if(waitingForRectangle) {
-				secondPoint = e.getPoint();
+			Point scaledPoint = new Point(e.getPoint().x / WorldView.TILE_SIZE,
+					e.getPoint().y / WorldView.TILE_SIZE);
+
+			System.out.println("Point clicked at pixel: " + scaledPoint.x
+					+ ", " + scaledPoint.y + " | Tile: " + scaledPoint.x + ", "
+					+ scaledPoint.y);
+
+			if (waitingForRectangle && firstPoint == null) {
+				firstPoint = scaledPoint;
+			} else if (waitingForRectangle) {
+				secondPoint = scaledPoint;
 			}
-			
-			if(waitingForPoint) {
-				firstPoint = e.getPoint();
+
+			if (waitingForPoint) {
+				firstPoint = scaledPoint;
 			}
+			myView.repaint();
 		}
 
 		@Override
 		public void mouseMoved(MouseEvent e) {
-			if(waitingForRectangle || waitingForPoint) {
-				currPoint = e.getPoint();
-				
+			if (waitingForRectangle || waitingForPoint) {
+				currPoint = new Point(e.getPoint().x / WorldView.TILE_SIZE,
+						e.getPoint().y / WorldView.TILE_SIZE);
+				myView.repaint();
 			}
 		}
-		
+
 	}
 
 }
