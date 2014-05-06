03e2b27003c4d07f84f7aa6825d8cda03b64ecc4
Zakerunner <zakerunner@gmail.com>
2014-05-06 01:51:42 -0700

    More work on main menu -won't draw image for some reason :(

--- Sciencers/src/view/MainMenuScreen.java
+++ Sciencers/src/view/MainMenuScreen.java
@@ -3,6 +3,7 @@ package view;
 import java.awt.FlowLayout;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.Toolkit;
 import java.io.File;
@@ -33,19 +34,28 @@ public class MainMenuScreen extends JFrame {
 		this.setTitle("Sciencers Demo");
 		this.setLayout(new FlowLayout());
 		this.setLocation(X_SCREEN_SIZE/2 - X_WINDOW_SIZE/2, Y_SCREEN_SIZE/2 - Y_WINDOW_SIZE/2);
-		
-		MainMenuPanel panel = new MainMenuPanel();
 	
+		MainMenuPanel panel = new MainMenuPanel();
+		panel.setLocation(0, 0);
+		panel.setSize(X_WINDOW_SIZE, Y_WINDOW_SIZE);
+		panel.setLayout(new GridLayout(1, 1));
+		panel.setVisible(true);
+		
+		this.add(panel);
+	
+//		panel.repaint();
 	}
 }
 
 class MainMenuPanel extends JPanel {
+	
+	private Image image;
+	
 	public MainMenuPanel() {
 		
 		try {
-			String s = "imgs/" + "Logo" + ".png";
-			Image i = ImageIO.read(new File(s));
-			
+			String s = "imgs/LogoCheezitMountain.jpg";
+			image = ImageIO.read(new File(s));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -53,7 +63,13 @@ class MainMenuPanel extends JPanel {
 	
 	@Override
 	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
 		Graphics2D g2 = (Graphics2D) g;
-		
+		g2.drawImage(image, 0, 0, null);
+	}
+	
+	//might be unnecessary
+	public void update() {
+		repaint();
 	}
 }
