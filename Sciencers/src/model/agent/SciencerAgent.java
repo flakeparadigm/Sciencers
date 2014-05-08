ed86403f095640d87f2fd1e5e65ba13c65502f64
Zakerunner <zakerunner@gmail.com>
2014-05-07 18:14:12 -0700

    Work on Sciencer Agent

--- Sciencers/src/model/agent/SciencerAgent.java
+++ Sciencers/src/model/agent/SciencerAgent.java
@@ -2,7 +2,15 @@ package model.agent;
 
 import java.awt.Point;
 
+import model.AlertCollection;
+import model.Entity;
+import model.World;
+import model.building.Building;
+import model.building.Lab;
 import model.inventory.Resource;
+import model.task.AgentDeath;
+import model.task.GoHereTask;
+import model.task.HarvestTreeTask;
 
 public class SciencerAgent extends AgentReplacement{
 
@@ -16,8 +24,45 @@ public class SciencerAgent extends AgentReplacement{
 	@Override
 	public void update() {
 		updateStats();
-		getNextTaskIfNotBusy(EAgent.SCIENCER);
+		
+		if (hunger < 0 && currentTask == null) {
+			currentTask = (new AgentDeath(this, new Point(
+					(int) currentPosition.x, (int) currentPosition.y)));
+			taskTimer = 0;
+		}
 
+		if (hunger < 0.5 * SEEK_FOOD_HUNGER) {
+			AlertCollection.addAlert("An agent is starving! D:");
+		}
+
+		// seek food if hungry
+		if (hunger < SEEK_FOOD_HUNGER && currentTask == null) {
+			if (findNearestTree(currentPosition) != null) {
+				currentTask = new HarvestTreeTask(this,
+						findNearestTree(currentPosition), World.terrain);
+				taskTimer = 10;
+			}
+		}
+		
+		Building lab = null;
+		
+		//if stats ok, then science!
+		for(Entity e : World.buildings) {
+			if(e instanceof Lab) {
+				lab = (Building) e;
+				break;
+			}
+		}
+		if(lab == null) {
+			AlertCollection.addAlert("Sciencers have no lab!");
+			return;
+		}
+		if(!currentPosition.equals(lab.getPos())) {
+			tasks.add(new GoHereTask(lab.getPos()));
+		}
+		//do research //TODO write this
+		
+		
 	}
 
 	@Override
